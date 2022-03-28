fd94ed876cb050f54e52c9b0fbbe59ee9cf4e45e
Adam McLane <mclanea92@gmail.com>
2022-03-28 16:43:36 -0400

    updated javascript with vars

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-var highScoresFinal = document.querySelector(highscores);
-console.log(highScoresFinal)
+var highScoresFinal = document.querySelector(highscoresFinal);
+var submitScore = document.querySelector(submit);
+var highScorePage = document.querySelector(highScoresFinal);
+var highScoreSubmit = document.querySelector(highscore-html);
+var beginButton = document.querySelector(beginButton);
