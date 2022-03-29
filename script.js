55f2493502790e200cfd6fe0567ade9bb3723740
Adam McLane <mclanea92@gmail.com>
2022-03-29 14:05:11 -0400

    updated javascript

--- script.js
+++ script.js
@@ -33,3 +33,17 @@ if(questions[0].choices[0]===questions[0].answer){
 else{
     timer--;
 }
+
+function startQuiz() {
+
+}
+
+function displayTimer(){
+  // adds a leading 0 when seconds remainder is less than 10
+  var sec = seconds % 60 < 10 ? "0" + seconds % 60 : seconds % 60;
+  // minutes calculation
+  var min = Math.floor(seconds / 60);
+
+  // put time on page
+  timer.textContent = `${min}:${sec}`;
+};
