3e11d50e2dd4dbf4d4818fb366680ba4c7c98688
Adam McLane <mclanea92@gmail.com>
2022-03-30 18:46:16 -0400

    added note

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ function startGame(){
   function displayQuestions() {
       
     
-        var answer1=document.querySelector('#a1');
+    var answer1=document.querySelector('#a1');
       var answer2=document.querySelector('#a2');
       var answer3=document.querySelector('#a3');
       var answer4=document.querySelector('#a4');
