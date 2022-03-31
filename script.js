4f5aa479f0c44fceb899cd507dfa1b9de4e873a6
Adam McLane <mclanea92@gmail.com>
2022-03-31 17:37:02 -0400

    updated javascript

--- script.js
+++ script.js
@@ -4,232 +4,143 @@ var timerInterval = '60';
 var start = document.querySelector('#beginButton')
 var questionHide = document.querySelector('.questions')
 var currentQuestNum = 0;
-
 var questions = [{
     ask: "Inside which HTML element do we put the JavaScript?",
     answers: ["<script>", "<javascript>", "<js>", "<scripting>"],
-    correctAnswer: 0
+    correctAnswer: "<script>"
 },
 {
     ask: "How do you create a function in JavaScript?",
     answers: ["function myFunction()", "function:myFunction()", "function = myFunction()", "func myFunction()"],
-    correctAnswer: 0
+    correctAnswer: "function myFunction()"
 },
 {
-    ask: "How do you call a function named “myFunction”?",
+    ask: "How do you call a function named 'myFunction' ?",
     answers: ["myFunction", "myFunction()", "call myFunction()", "call function myFunction()"],
-    correctAnswer: 1
+    correctAnswer: "myFunction()"
 },
 {
     ask: "How do you start an IF statement in JavaScript?",
     answers: ["if i = 5", "if i == 5 then", "if i = 5 then", "if (i == 5)"],
-    correctAnswer: 3
+    correctAnswer: "if (i == 5)"
 },
 {
     ask: "How do you start a FOR loop in JavaScript?",
     answers: ["for i = 1 to 5", "for (i =0; i <= 5; i++)", "for (i <= 5; i++)", "for (i = 0; i <= 5)"],
-    correctAnswer: 1
+    correctAnswer: "for (i =0; i <= 5; i++)"
 },
 {
     ask: "How can you add a comment in JavaScript?",
-    answers: ["‘This is a comment", "<!--This is a comment -->", "//This is a comment", "**This is a comment**"],
-    correctAnswer: 2
+    answers: ["'This is a comment'", "<!--This is a comment -->", "//This is a comment", "**This is a comment**"],
+    correctAnswer: "//This is a comment"
 },
 {
     ask: "What is the correct way to write a JavaScript array?",
-    answers: ["var colors = (1:”red”, 2:”green”, 3:”blue”)", "var colors = 1 = (“red”), 2 = (“green”), 3 = (“blue”)", "var colors = “red”, ”green”, ”blue”", "var colors = [“red”, “green”, “blue”]"],
-    correctAnswer: 3
+    answers: ["var colors = (1:'red', 2:'green', 3:'blue')", "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')", "var colors = 'red', 'green', 'blue'", "var colors = ['red', 'green', 'blue']"],
+    correctAnswer: "var colors = ['red', 'green', 'blue']"
 },
 {
     ask: "How do you round the number 7.25, to the nearest integer?",
     answers: ["round (7.25)", "Math.rnd(7.25)", "Math.round(7.25)", "rnd(7.25)"],
-    correctAnswer: 2
+    correctAnswer: "Math.round(7.25)"
 },
 {
     ask: "What event occurs when the user clicks on an HTML element?",
     answers: ["onmouseclick", "onchange", "onmouseover", "onclick"],
-    correctAnswer: 3
+    correctAnswer: "onclick"
 },
 {
     ask: "How do you declare a JavaScript variable?",
     answers: ["v carName;", "var carName;", "variable carName;", "v (carName);"],
-    correctAnswer: 1
+    correctAnswer: "var carName;"
 },
 {
     ask: "Which operator is used to assign a value to a variable?",
     answers: ["x", "*", "-", "="],
-    correctAnswer: 3
+    correctAnswer: "="
 }
 ]
 var correct = 0;
 var wrong = 0;
-for(var i = 0; i > questions.length; i=i+1){ // we flipped the > from <
+for (var i = 0; i > questions.length; i = i + 1) { // we flipped the > from <
     var userChoice = questions[i].choices[0];
-    if(userChoice===questions[i].answer){
+    if (userChoice === questions[i].answer) {
         ++correct;
     }
-    else{
-        ++wrong; 
+    else {
+        ++wrong;
     }
 }
 console.log(correct);
-console.log(wrong); 
-
-beginButton.addEventListener('click', function() {
-    console.log('button 1')    
+console.log(wrong);
+beginButton.addEventListener('click', function () {
+    console.log('button 1')
     // var timer =setInterval(function () {
     //         --timer;
-    //         // timer = currentTime;   
+    //         // timer = currentTime;
     //         // checkTime(timerStart);
     //     }, 1000)
-    
-        instructions.style.display = 'none';
-        questionHide.classList.add('questions-show');
-        beginButton.style.display = 'none';
-    
-        return startGame();
-    })
+    instructions.style.display = 'none';
+    questionHide.classList.add('questions-show');
+    beginButton.style.display = 'none';
+    return startGame();
+})
 //timer code
-function startGame(){
+function startGame() {
     // important to clear out previous interval before starting a new one
     clearInterval(timerInterval);
     seconds = 60;
-    timerInterval = setInterval(function(){
-      console.log(--seconds);
-  
-      displayTimer();
-  
-      if(seconds <= 0){
-        clearInterval(timerInterval);
-      }
+    timerInterval = setInterval(function () {
+        console.log(--seconds);
+        displayTimer();
+        if (seconds <= 0) {
+            clearInterval(timerInterval);
+            questionHide.style.display = 'none';
+            var endSection = document.querySelector('.highscore-html')
+            endSection.style.display = 'block';
+        }
     }, 1000);
-    
     displayQuestions();
-  }
-  function displayTimer(){
+}
+function displayTimer() {
     // adds a leading 0 when seconds remainder is less than 10
     var sec = seconds % 60 < 10 ? "0" + seconds % 60 : seconds % 60;
     // minutes calculation
     var min = Math.floor(seconds / 60);
-  
     // put time on page
     time.textContent = `${min}:${sec}`;
-  };
-
-  function displayQuestions() {
-      
+};
+function displayQuestions() {
     var answerTags = document.querySelectorAll('#a1, #a2, #a3, #a4')
-      var question=document.querySelector('.question');
-
-      for(var i =0 ; i < 4; i++) {
-          answerTags[i].textContent=questions[currentQuestNum].answers[i]
-      
-          if (i === questions[currentQuestNum].correctAnswer) {
-          answerTags[i].setAttribute('data-Correct', true)
-      } else {
-        answerTags[i].setAttribute('data-Correct', false)
-      }
+    var question = document.querySelector('.question');
+    for (var i = 0; i < 4; i++) {
+        answerTags[i].textContent = questions[currentQuestNum].answers[i]
+        if (i === questions[currentQuestNum].correctAnswer) {
+            answerTags[i].setAttribute('data-Correct', true)
+        } else {
+            answerTags[i].setAttribute('data-Correct', false)
         }
-      
-
-      question.textContent=questions[currentQuestNum].ask
-
-      return
-  }
-
-
-
-
-
-  //   start.addEventListener("click", function(beginButton){
-//     console.log('button 2') 
-//     var timer =setInterval(function () {
-//         --timer;
-//         // timer = currentTime;   
-//         // checkTime(timerStart);
-//     }, 1000)
-
-//     instructions.style.display = 'none';
-//     questionHide.style.display = 'block';
-//     // beginButton.style.display = 'none';
-
-//     startGame();
-//   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-// var highScoresFinal = document.querySelector(".highscoresFinal");
-// var submitScore = document.querySelector(".submit");
-// var highScorePage = document.querySelector(".highScoresFinal");
-// var highScoreSubmit = document.querySelector(".highscore-html");
-// var beginButton = document.querySelector("#beginButton");
-// var choicesBox = document.querySelector('.choiceanswers');
-
-// var quest = document.querySelector('.question');
-// var question = document.querySelector('.choiceanswers')
-
-
-
-
-
-
-// // I put the questions into a var objects below to pull the answers
-// // and the correct one
-//FROM TOM
-// var ranQuest = questions;
-// var questions = [
-//     {
-//         question:"what is my name?", 
-//         choices:["Tom", "adam"],
-//         answer:"Tom"
-//     }, 
-//     {
-//         question:"what is your eye color", 
-//         choices:["green", "blue"],
-//         answer:"blue"
-//     }
-// ]
-
-// questions[0].question; //"what is your name?"
-// questions[0].choices; //["Tom", "adam"]
-// questions[0].choices[0]; //"Tom"
-// var correctAnswers = 0;
-// var timer = 60;
-// if(questions[0].choices[0]===questions[0].answer){
-//     correctAnswers++;
-// }
-// else{
-//     timer--;
-// }
-
-//
-
-// function startGame() {
-//     time = 60;
-    
-//     multiQuest ()
-//     startTimer ()
-// }
-
-// function StartTimer() {
-//     time = setInterval(funtion(timer)) 
-//         timeCount-- ;
-//         time.textContent = time;
-//     }
-
-
-
+        answerTags[i].addEventListener("click", checkAnswer);
+    }
+    question.textContent = questions[currentQuestNum].ask
+}
+ function checkAnswer(event) {
+     var userChoice = event.target.innerText;
+     var correctAnswer = questions[currentQuestNum].correctAnswer;
+     if (userChoice === correctAnswer) {
+         alert("You answered correctly!")
+     }
+     else {
+         alert("That was the wrong answer :(")
+         seconds-=10;
+     }
+     currentQuestNum++;
+     if (currentQuestNum < questions.length) {
+         displayQuestions();
+     } else {
+        clearInterval(timerInterval);
+        questionHide.style.display = 'none';
+        var endSection = document.querySelector('.highscore-html')
+        endSection.style.display = 'block';
+     }
+ }
